be28a38db56269752b659d80a239830f83b1c86e
swoopertr <tunc.kiral@gmail.com>
2025-03-22 03:55:35 -0400

    adding img and counting likes and dislikes

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 console.log('Hello!');
+
+let likeText = document.getElementById('likes');
+let dislikeText = document.getElementById('dislikes');
+
+let likesCount = 0;
+let dislikesCount = 0;
+
+
+function reloadImg(){
+    let img = document.getElementById('duck-img');
+    img.src = 'https://random-d.uk/api/randomimg';
+}
+
+document.addEventListener('keydown', function(event){
+    
+    if(event.key === 'ArrowLeft'){
+        dislikesCount++;
+        console.log('ArrowLeft ',dislikesCount);
+        dislikeText.textContent =`sevmediklerim : ${dislikesCount}`;
+        reloadImg();
+    }else if(event.key === 'ArrowRight'){
+        likesCount++;
+        console.log('ArrowRight ', likesCount);
+        likeText.textContent = `sevdiklerim : ${likesCount}`;
+        reloadImg();
+    }
+
+});
